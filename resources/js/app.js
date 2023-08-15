2f313f2c061dfb7bb92d5caa1fb4ddde3b864c99
József Bagota <jozsef.bagota@gmail.com>
2023-08-15 20:13:16 +0200

    changing the require vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ require('./bootstrap');
 
 window.Vue = require('vue').default
 
-require('fruits-website-vue-components').default.install(Vue);
+require('fruits-vue-components').default.install(Vue);
 
 const app = new Vue({
     el: '#app',
