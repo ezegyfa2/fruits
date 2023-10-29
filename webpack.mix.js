126cc3ed4814b2486002538dfec2e39bdd454f28
ezegyfa2 <darvasvok@gmail.com>
2023-10-29 05:48:57 +0200

    Fixing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
 const mix = require('laravel-mix')
 const path = require('path')
+const { size } = require('lodash')
+require('laravel-mix-compress')
+require('helper-vue-components/AssetCompiler.js')
 
 global.currentWebpackConfig = {
     module: {
@@ -21,11 +24,22 @@ global.currentWebpackConfig = {
         publicPath: '/',
     }
 }
+
 mix.webpackConfig(currentWebpackConfig)
-
-//mix.js('resources/js/basicPackages.js', 'public/js').vue()
-mix.js('resources/js/welcome.js', 'public/js').vue()
-mix.js('resources/js/products.js', 'public/js').vue()
+//mix.js('resources/js/welcome.js', 'public/js').vue()
 //mix.sass('resources/sass/welcome.scss', 'public/css/')
-//mix.copy('node_modules/electrician-vue-components/src/Welcome/CompiledTemplate.json', 'app/Templates/Welcome.json')
+/*mix.copy(
+    path.join('node_modules', 'dynamic-web-vue-components', 'src', 'Welcome', 'CompiledTemplate.json'), 
+    path.join('app', 'Templates', 'Welcome.json')
+)*/
 
+//mix.js('resources/js/products.js', 'public/js').vue()
+//mix.sass('resources/sass/products.scss', 'public/css/')
+//compileAssets(mix, 'fruits-vue-components', [ 'Products' ])
+compileAssets(mix, 'fruits-vue-components', [ 'Welcome' ])
+//replaceComponentRegistrations('./node_modules/helper-vue-components/src')
+
+//headerBackgroundImageGenerator.generate()
+//articleImageGenerator.generate()
+//latestWorkImageGenerator.generate()
+//replaceImageCache('app/Http/ImageCache.php')
