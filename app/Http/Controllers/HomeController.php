ba9ce13233b2759d49245641e4e5ad92a4b77f46
ezegyfa2 <darvasvok@gmail.com>
2023-10-29 05:31:24 +0200

    Fixing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,44 +4,33 @@ namespace App\Http\Controllers;
 
 use Ezegyfa\LaravelHelperMethods\DynamicTemplateMethods;
 use Illuminate\Support\Facades\App;
-use stdClass;
 
 class HomeController extends Controller
 {
     public function welcome() {
-        $templateParams = new stdClass;
-        $templateParams->current_language = strtoupper(App::currentLocale());
-        $templateParams->current_url = "";
         return DynamicTemplateMethods::getTranslatedTemplateDynamicPage(
             'fruits_welcome',
-            'node_modules/fruits-vue-components/src/Welcome/CompiledTemplate.json',
-            $templateParams, 
+            'Welcome',
+            DynamicTemplateMethods::getTemplateLayoutParams(), 
             [
                 'welcome',
             ],
             [ 
-                'fontawesome/css/fontawesome.min', 
-                'fontawesome/css/brands.min',
-                'fontawesome/css/solid.min' 
+                'welcome',
             ]
         );
     }
 
     public function products() {
-        $templateParams = new stdClass;
-        $templateParams->current_language = strtoupper(App::currentLocale());
-        $templateParams->current_url = "/products";
         return DynamicTemplateMethods::getTranslatedTemplateDynamicPage(
             'fruits_products',
-            'node_modules/fruits-vue-components/src/Products/CompiledTemplate.json', 
-            $templateParams, 
+            'Products', 
+            DynamicTemplateMethods::getTemplateLayoutParams(), 
             [
                 'products',
             ],
             [ 
-                'fontawesome/css/fontawesome.min', 
-                'fontawesome/css/brands.min',
-                'fontawesome/css/solid.min' 
+                'products'
             ]
         );
     }
